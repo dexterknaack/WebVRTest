95ad1a7444ee19214ae0ee19bf3914e90067c76d
dexterknaack <dexter.knaack@gmail.com>
2021-09-27 18:44:11 -0700

    New Lines ;)

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('rebeccapurple')
+scene.background = new THREE.Color('pink')
 // scene.fog = new THREE.Fog('white',1,1000);
 
 
@@ -48,10 +48,11 @@ const sphere = new THREE.Mesh(sphereGeometry , sphereMaterial)
 scene.add(sphere)*/
 
 
-const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+/*const torusGeometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
 const torusMaterial = new THREE.MeshBasicMaterial( { color: 'cyan',wireframe: true } );
 const torusKnot = new THREE.Mesh( torusGeometry, torusMaterial )
-scene.add( torusKnot )
+scene.add( torusKnot )*/
+
 
 
 const geometry = new THREE.PlaneGeometry( 100, 100 );
@@ -97,11 +98,62 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.xr.enabled = true;
 document.body.appendChild( VRButton.createButton( renderer ) );
 
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const Fpoints = [];
+const Upoints = [];
+const Cpoints = [];
+const Kpoints = [];
+Fpoints.push( new THREE.Vector3( - 10, 1, 0 ) ); //F
+Fpoints.push( new THREE.Vector3( -10, 5, 0 ) );
+Fpoints.push( new THREE.Vector3( -8, 5, 0 ) );
+Fpoints.push( new THREE.Vector3( -10, 5, 0 ) );
+Fpoints.push( new THREE.Vector3( -10, 3, 0 ) );
+Fpoints.push( new THREE.Vector3( -8, 3, 0 ) );
+Fpoints.push( new THREE.Vector3( -10, 3, 0 ) );
+Upoints.push( new THREE.Vector3( -7, 1, 0 ) ); //U
+Upoints.push( new THREE.Vector3( -7, 5, 0 ) );
+Upoints.push( new THREE.Vector3( -7, 1, 0 ) );
+Upoints.push( new THREE.Vector3( -5, 1, 0 ) );
+Upoints.push( new THREE.Vector3( -5, 5, 0 ) );
+Upoints.push( new THREE.Vector3( -5, 1, 0 ) );
+Cpoints.push( new THREE.Vector3( -4, 1, 0 ) ); //C
+Cpoints.push( new THREE.Vector3( -4, 5, 0 ) );
+Cpoints.push( new THREE.Vector3( -2, 5, 0 ) );
+Cpoints.push( new THREE.Vector3( -4, 5, 0 ) );
+Cpoints.push( new THREE.Vector3( -4, 1, 0 ) );
+Cpoints.push( new THREE.Vector3( -2, 1, 0 ) );
+Cpoints.push( new THREE.Vector3( -4, 1, 0 ) );
+Kpoints.push( new THREE.Vector3( -1, 1, 0 ) ); //k
+Kpoints.push( new THREE.Vector3( -1, 5, 0 ) );
+Kpoints.push( new THREE.Vector3( -1, 3, 0 ) );
+Kpoints.push( new THREE.Vector3( 1, 5, 0 ) );
+Kpoints.push( new THREE.Vector3( -1, 3, 0 ) );
+Kpoints.push( new THREE.Vector3( 1, 1, 0 ) );
+const lineMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } )
+const FmeshGeometry = new THREE.BufferGeometry().setFromPoints( Fpoints );
+const Fline = new THREE.Line( FmeshGeometry, lineMaterial )
+scene.add( Fline )
+
+const UmeshGeometry = new THREE.BufferGeometry().setFromPoints( Upoints );
+const Uline = new THREE.Line( UmeshGeometry, lineMaterial )
+scene.add( Uline )
+
+const CmeshGeometry = new THREE.BufferGeometry().setFromPoints( Cpoints );
+const Cline = new THREE.Line( CmeshGeometry, lineMaterial )
+scene.add( Cline )
+
+const KmeshGeometry = new THREE.BufferGeometry().setFromPoints( Kpoints );
+const Kline = new THREE.Line( KmeshGeometry, lineMaterial )
+scene.add( Kline )
+renderer.render( scene, camera )
 
 /**
  * Animate
